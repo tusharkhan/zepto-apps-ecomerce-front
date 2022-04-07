ef61b4caa7df2c974d0ad5d8cc6ddd883081e520
tushar <tushar.khan0122@gmail.com>
2022-04-08 03:12:05 +0600

    user registration ok

--- config/api.php
+++ config/api.php
@@ -7,7 +7,7 @@
 
 
 return [
-    'admin_endpoint' => 'http://localhost:8080/api/admin/',
-    'user_endpoint' => 'http://localhost:8080/api/user/',
-    'api_endpoint' => 'http://localhost:8080/api/',
+    'admin_endpoint' => env('admin_endpoint', 'http://localhost:8080/api/admin/'),
+    'user_endpoint' => env('user_endpoint', 'http://localhost:8080/api/user/'),
+    'api_endpoint' => env('API_ENDPOINT', 'http://localhost:8080/api/'),
 ];
